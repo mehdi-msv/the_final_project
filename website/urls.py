d76e2e9e8333b043c2ff8a904aa39f441a182f04
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-06-30 15:23:52 +0330

    created base.html

--- website/urls.py
+++ website/urls.py
@@ -8,7 +8,6 @@ app_name = 'website'
 urlpatterns = [
     path('',TemplateView.as_view(template_name='website/index.html'),name='index'),
     path('about',TemplateView.as_view(template_name='website/about.html'),name='about'),
-    path('shop',TemplateView.as_view(template_name='website/shop.html'),name='shop'),
     path('coffees',TemplateView.as_view(template_name='website/coffees.html'),name='coffees'),
     path('contact',contact_view,name='contact'),
 ]
