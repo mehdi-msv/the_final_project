863d617a53c5299ba813451d70795868fa0690cf
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-07-02 19:49:31 +0330

    change template

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,5 @@ app_name = 'website'
 urlpatterns = [
     path('',TemplateView.as_view(template_name='website/index.html'),name='index'),
     path('about',TemplateView.as_view(template_name='website/about.html'),name='about'),
-    path('coffees',TemplateView.as_view(template_name='website/coffees.html'),name='coffees'),
     path('contact',contact_view,name='contact'),
 ]
